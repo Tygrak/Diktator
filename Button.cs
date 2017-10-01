7be576c29b14cccea46b39f5869221130d440f82
Tygrak <Tygrak@users.noreply.github.com>
2017-10-01 17:04:02 +0200

    Moved to SharpCanvas 2.2

--- Button.cs
+++ Button.cs
@@ -10,11 +10,12 @@ namespace Diktator{
         public static GlyphMap glyphMap;
         public int value;
         public Action OnClickAction;
-        public Sprite3D buttonSprite;
+        public ShadedSprite buttonSprite;
         public DynamicText buttonText;
 
         public Button(OpenTK.Vector3 pos, OpenTK.Vector3 scale){
-            buttonSprite = new Sprite3D(pos, scale, buttonTexture);
+            buttonSprite = new ShadedSprite(pos, scale, buttonTexture, Diktator.customShaderId);
+            buttonSprite.shaderUniforms.Add(3, new UniformVec3(new OpenTK.Vector3(1, 1, 1)));
             buttonText = new DynamicText(pos, new OpenTK.Vector3(scale.X*0.125f, scale.Y*0.4f, 1f), "testButton", glyphMap);
             buttonText.AlignText(TextAlignment.Centralized);
         }
@@ -22,8 +23,10 @@ namespace Diktator{
         public bool CheckHit(OpenTK.Vector2 clickPosition){
             if(clickPosition.X > buttonSprite.position.X - buttonSprite.scale.X/2 && clickPosition.X < buttonSprite.position.X + buttonSprite.scale.X/2 && 
                clickPosition.Y > buttonSprite.position.Y - buttonSprite.scale.Y/2 && clickPosition.Y < buttonSprite.position.Y + buttonSprite.scale.Y/2){
+                buttonSprite.shaderUniforms[3] = new UniformVec3(new OpenTK.Vector3(0.5f, 0.5f, 0.5f));
                 return true;
             }
+            buttonSprite.shaderUniforms[3] = new UniformVec3(new OpenTK.Vector3(1f, 1f, 1f));
             return false;
         }
     }
--- Diktator.cs
+++ Diktator.cs
@@ -6,10 +6,11 @@ using System.Drawing.Imaging;
 
 namespace Diktator{
     class Diktator{
+        public static int customShaderId;
         SharpCanvasWindow scWindow;
         MainWindow mainWindow;
         Sprite3D selectionMarker;
-        Sprite3D questionDiv;
+        ShadedSprite questionDiv;
         DynamicText questionText;
         Button[] buttons = new Button[4];
         Bitmap canvas;
@@ -21,12 +22,36 @@ namespace Diktator{
             scWindow.Start(60, 60);
             mainWindow = scWindow.Window;
             mainWindow.SetClearColor(OpenTK.Color.White);
+            customShaderId = mainWindow.LoadShaderFromString(@"#version 410 core
+                #extension GL_ARB_explicit_uniform_location : require
+                layout (location = 20) uniform  mat4 projection;
+                layout (location = 21) uniform  mat4 modelView;
+                layout (location = 3) uniform vec3 tint;
+                layout (location = 2) in vec2 textureCoord;
+                layout (location = 1) in vec4 position;
+                out vec2 frag_textureCoord;
+                out vec3 frag_tint;
+                void main(void){
+                    gl_Position = projection * modelView * position;
+                    frag_textureCoord = textureCoord;
+                    frag_tint = tint;
+                }", @"#version 410 core
+                #extension GL_ARB_explicit_uniform_location : require
+                in vec2 frag_textureCoord;
+                in vec3 frag_tint;
+                uniform sampler2D textureObject;
+                out vec4 color;
+                void main(void){
+                    color = texture(textureObject, frag_textureCoord);
+                    color = color + (vec4(frag_tint, 1)*color.a);
+                }");
             int markerTexture = mainWindow.LoadTexture("x.png");
             Button.buttonTexture = mainWindow.LoadTexture("button.png");
             Button.glyphMap = new GlyphMap(GlyphMap.czechGlyphs, 64);
             Button.glyphMap.texture = mainWindow.LoadTextureFromBitmap(Button.glyphMap.bitmap);
             selectionMarker = new Sprite3D(new OpenTK.Vector3(0, 0, 10), new OpenTK.Vector3(0.03f, 0.03f, 1), markerTexture);
-            questionDiv = new Sprite3D(new OpenTK.Vector3(0, 0.5f, -2), new OpenTK.Vector3(1.6f, 0.85f, 1), Button.buttonTexture);
+            questionDiv = new ShadedSprite(new OpenTK.Vector3(0, 0.5f, -2), new OpenTK.Vector3(1.6f, 0.85f, 1), Button.buttonTexture, customShaderId);
+            questionDiv.shaderUniforms.Add(3, new UniformVec3(new OpenTK.Vector3(1, 1, 1)));
             questionText = new DynamicText(new OpenTK.Vector3(0, 0.5f, -2), new OpenTK.Vector3(0.08f, 0.2f, 1), "Kolik sou 1+1???", Button.glyphMap);
             questionText.AlignText(TextAlignment.Centralized);
             buttons[0] = new Button(new OpenTK.Vector3(0.5f, -0.25f, -2), new OpenTK.Vector3(0.8f, 0.3f, 1));
